08c5d5e5c121d4d4fa1542d0781cf10477c756d2
MatiasUrielGluck <matiasugluck@gmail.com>
2023-01-14 21:48:09 -0300

    Map section finished

--- src/pages/home/components/MapSection.jsx
+++ src/pages/home/components/MapSection.jsx
@@ -1,9 +1,20 @@
-import { StyledMapSection } from "../styled-components"
+import { StyledMapSection } from "../styled-components";
 
 export const MapSection = () => {
   return (
     <StyledMapSection>
-        <h1>Visitá nuestro local</h1>
+      <h1>Visitá nuestro local</h1>
+
+      <div className="map-container">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1380.7627564967306!2d-58.39822671464821!3d-34.60350939814087!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccaecaec40c35%3A0x1090754610e2cbb8!2sTextil%20Gluck!5e0!3m2!1ses-419!2sar!4v1673743181774!5m2!1ses-419!2sar"
+          width="600"
+          height="450"
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
     </StyledMapSection>
-  )
-}
+  );
+};
--- src/pages/home/styled-components/StyledMapSection.js
+++ src/pages/home/styled-components/StyledMapSection.js
@@ -1,5 +1,23 @@
 import styled from "styled-components";
 
 export const StyledMapSection = styled.div`
+  padding: 24px;
 
+  h1 {
+    text-align: center;
+    margin-bottom: 24px;
+  }
+
+  .map-container {
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: center;
+    justify-content: center;
+
+    iframe {
+      border: none;
+      width: 100%;
+      max-width: 900px;
+    }
+  }
 `;
